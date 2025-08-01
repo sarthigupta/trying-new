5196e69ae02cd4280034fd028ab16484406fca08
sarthigupta <workwithsarthi@gmail.com>
2025-08-01 20:50:53 +0530

    do it

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css"
 import Navigation from "./Components/navigation.jsx"
 import Hero from "./Components/Hero.jsx"
-import New from "./Components/New.jsx";
+
 
 function App(){
   return (
@@ -9,7 +9,7 @@ function App(){
     <>
     <Navigation></Navigation>
     <Hero></Hero>
-    <New></New>
+    
     
     </>
     
