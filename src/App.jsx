83483d4ab008cdd759feef6bf92f8ccef5c9c562
sarthigupta <workwithsarthi@gmail.com>
2025-08-01 20:43:28 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css"
 import Navigation from "./Components/navigation.jsx"
 import Hero from "./Components/Hero.jsx"
-import New from "./Components/New.jsx";
-import AgainCheck from "./Components/AgainCheck.jsx";
+
 function App(){
   return (
 
