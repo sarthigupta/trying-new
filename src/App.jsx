1b5806239a4eab0eb9be8e2f916e83f58d3347db
sarthigupta <workwithsarthi@gmail.com>
2025-08-01 16:10:29 +0530

    first push done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css"
 import Navigation from "./Components/navigation.jsx"
 import Hero from "./Components/Hero.jsx"
+import New from "./Components/New.jsx";
 function App(){
   return (
 
     <>
     <Navigation></Navigation>
     <Hero></Hero>
+    <New></New>
     </>
     
   ) 
---
+++ src/Components/New.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const New = () => {
+  return (
+    <div>Checking here for something new!</div>
+  )
+}
+
+export default New
